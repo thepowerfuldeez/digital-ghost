7bf2a4cb8bee81cfe9f4432691ec3fd461bc9a59
Konstantin <Konstantin.Poleshchuk@bercut.com>
2019-09-27 21:47:20 +0300

    feat: group parsing

--- vkparser/src/config.ts
+++ vkparser/src/config.ts
@@ -6,5 +6,5 @@ export default {
     password: "7kB5RqW8wQ572nGOUmp5t4",
   },
   token:
-    "8bf5db558bf5db558bf5db55418bad152488bf58bf5db55d7f8b0e1164ed9ce2b87d4aa",
+    "64c45944813b541d7d80c5428cb103e39e98da1001b2000e54500adf7c05f190c50742aeca8957db10a9e",
 };
