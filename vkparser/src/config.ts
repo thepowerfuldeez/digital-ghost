f442b81d68971c6294925c68b76ad657dd753b70
Konstantin <Konstantin.Poleshchuk@bercut.com>
2019-09-27 20:46:59 +0300

    add config for mongo

---
+++ vkparser/src/config.ts
@@ -0,0 +1,10 @@
+export default {
+  mongo: {
+    url: "mongodb://95.213.38.3:27017",
+    dbName: "vk-data",
+    username: "root",
+    password: "7kB5RqW8wQ572nGOUmp5t4",
+  },
+  token:
+    "8bf5db558bf5db558bf5db55418bad152488bf58bf5db55d7f8b0e1164ed9ce2b87d4aa",
+};
