435a1d70ce995f391d210c85c5884c694ba35488
Konstantin <Konstantin.Poleshchuk@bercut.com>
2019-09-27 20:48:40 +0300

    feat: add connection to mongo

--- vkparser/src/index.ts
+++ vkparser/src/index.ts
@@ -1,4 +1,35 @@
-const helloFn = (name: string): string =>
-  `Здарова ${name}, сегодня мы будем писать на ноде с тайпскриптом!`;
+import { VK } from "vk-io";
+import config from "./config";
+import mongodb, { MongoClient } from "mongodb";
 
-console.log(helloFn("щеглы"));
+const parser = new VK({
+  token: config.token,
+  apiMode: "parallel",
+});
+
+const client = new MongoClient(config.mongo.url, {
+  useNewUrlParser: true,
+  reconnectInterval: 1000,
+  reconnectTries: Number.MAX_VALUE,
+  auth: {
+    user: config.mongo.username,
+    password: config.mongo.password,
+  },
+});
+
+async function connectToDb() {
+  const connection = await client.connect();
+  const db = await connection.db(config.mongo.dbName);
+  return db;
+}
+
+async function getSubjects() {
+  const db = await connectToDb();
+  const subjects = db.collection("subjects");
+
+  const res = await subjects.find({}).toArray();
+
+  return res;
+}
+
+getSubjects().then(console.log);
