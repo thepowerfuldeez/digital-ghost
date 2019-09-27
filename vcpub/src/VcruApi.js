3821ff9960cb605f1054d62fc4770bce126c5bcc
marsgpl <marsgpl@gmail.com>
2019-09-28 00:01:15 +0300

    wip

--- vcpub/src/VcruApi.js
+++ vcpub/src/VcruApi.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const wait = require('./wait');
 
 module.exports = class {
     constructor(conf) {
@@ -27,6 +28,8 @@ module.exports = class {
             return res.json();
         });
 
+        await wait(this.conf.waitAfterEachCallMs);
+
         return {
             headers,
             response,
@@ -34,140 +37,125 @@ module.exports = class {
     }
 
     async possess(subsiteId) {
-        this.possessToken = ....
-    }
+        const result = await this.call('/auth/possess', {
+            id: subsiteId,
+        });
 
-    async createPost(post) {
-        return {
-            id: ,
-            url: ,
+        this.possessToken = result.headers['X-Device-Possession-Token'.toLowerCase()];
+
+        if (!this.possessToken) {
+            throw 'possess failed: result=' + JSON.stringify(result);
         }
     }
 
-    async createComment(comment) {
-        return {
-            id: ,
-        }
-    }
+    /**
+     * post.subsiteId
+     * post.title
+     * post.text
+     */
+    // async createPost(post) {
+    //     return {
+    //         id: ,
+    //         url: ,
+    //     }
+    // }
 
-    async likePost(postId, sign) {
+    // async createComment(comment) {
+    //     return {
+    //         id: ,
+    //     }
+    // }
 
-    }
+    // async likePost(postId, sign) {
 
-    async likeComment(commentId, sign) {
+    // }
 
-    }
+    // async likeComment(commentId, sign) {
+
+    // }
 };
 
 
 
-    async vcLike(dataset) {
-        const apiPath = '/like';
-        const params = new URLSearchParams;
+    // async vcLike(dataset) {
+    //     const apiPath = '/like';
+    //     const params = new URLSearchParams;
 
-        params.append('id', dataset.entityId);
-        params.append('type', dataset.type);
-        params.append('sign', dataset.sign);
+    //     params.append('id', dataset.entityId);
+    //     params.append('type', dataset.type);
+    //     params.append('sign', dataset.sign);
 
-        const result = await fetch(this.conf.vcru.apiHost + apiPath, {
-            method: 'POST',
-            body: params,
-            headers: {
-                'X-Device-Token': this.conf.vcru.apiToken,
-            },
-        }).then(res => res.json());
+    //     const result = await fetch(this.conf.vcru.apiHost + apiPath, {
+    //         method: 'POST',
+    //         body: params,
+    //         headers: {
+    //             'X-Device-Token': this.conf.vcru.apiToken,
+    //         },
+    //     }).then(res => res.json());
 
-        console.log('result like:', result);
-    }
+    //     console.log('result like:', result);
+    // }
 
-    async vcCreateComment(comment) {
-        const apiPath = '/comment/add';
-        const params = new URLSearchParams;
+    // async vcCreateComment(comment) {
+    //     const apiPath = '/comment/add';
+    //     const params = new URLSearchParams;
 
-        params.append('id', comment.postId);
-        params.append('text', comment.text);
+    //     params.append('id', comment.postId);
+    //     params.append('text', comment.text);
 
-        if (comment.parentCommentId) {
-            params.append('reply_to', comment.parentCommentId);
-        }
+    //     if (comment.parentCommentId) {
+    //         params.append('reply_to', comment.parentCommentId);
+    //     }
 
-        if (comment.attachments) {
-            // params.append('attachments', JSON.stringify([ { type:'image',data:{.....} } ]));
-        }
+    //     if (comment.attachments) {
+    //         // params.append('attachments', JSON.stringify([ { type:'image',data:{.....} } ]));
+    //     }
 
-        const result = await fetch(this.conf.vcru.apiHost + apiPath, {
-            method: 'POST',
-            body: params,
-            headers: {
-                'X-Device-Token': this.conf.vcru.apiToken,
-            },
-        }).then(res => res.json());
+    //     const result = await fetch(this.conf.vcru.apiHost + apiPath, {
+    //         method: 'POST',
+    //         body: params,
+    //         headers: {
+    //             'X-Device-Token': this.conf.vcru.apiToken,
+    //         },
+    //     }).then(res => res.json());
 
-        if (!result || !result.result || !result.result.id) {
-            throw 'commenting failed: result=' + JSON.stringify(result)
-                + '; comment=' + JSON.stringify(comment);
-        }
+    //     if (!result || !result.result || !result.result.id) {
+    //         throw 'commenting failed: result=' + JSON.stringify(result)
+    //             + '; comment=' + JSON.stringify(comment);
+    //     }
 
-        return {
-            id: result.result.id,
-        };
-    }
+    //     return {
+    //         id: result.result.id,
+    //     };
+    // }
 
-    async vcCreatePost(post) {
-        const apiPath = '/entry/create';
-        const params = new URLSearchParams;
+    // async vcCreatePost(post) {
+    //     const apiPath = '/entry/create';
+    //     const params = new URLSearchParams;
 
-        params.append('title', post.title);
-        params.append('subsite_id', this.conf.vcru.subsiteId);
-        params.append('text', post.text);
+    //     params.append('title', post.title);
+    //     params.append('subsite_id', this.conf.vcru.subsiteId);
+    //     params.append('text', post.text);
 
-        if (post.attachments) {
-            // params.append('attachments', JSON.stringify([ { type:'image',data:{.....} } ]));
-        }
+    //     if (post.attachments) {
+    //         // params.append('attachments', JSON.stringify([ { type:'image',data:{.....} } ]));
+    //     }
 
-        const result = await fetch(this.conf.vcru.apiHost + apiPath, {
-            method: 'POST',
-            body: params,
-            headers: {
-                'X-Device-Token': this.conf.vcru.apiToken,
-            },
-        }).then(res => res.json());
+    //     const result = await fetch(this.conf.vcru.apiHost + apiPath, {
+    //         method: 'POST',
+    //         body: params,
+    //         headers: {
+    //             'X-Device-Token': this.conf.vcru.apiToken,
+    //         },
+    //     }).then(res => res.json());
 
-        if (!result || !result.result || !result.result.id) {
-            throw 'posting failed: result=' + JSON.stringify(result)
-                + '; post=' + JSON.stringify(post);
-        }
+    //     if (!result || !result.result || !result.result.id) {
+    //         throw 'posting failed: result=' + JSON.stringify(result)
+    //             + '; post=' + JSON.stringify(post);
+    //     }
 
-        return {
-            id: result.result.id,
-            url: result.result.url,
-        };
-    }
-
-    async vcAuth() {
-        const apiPath = '/auth/possess';
-        const params = new URLSearchParams;
-
-        params.append('id', this.conf.vcru.subsiteId);
-
-        let responseHeaders;
-        const result = await fetch(this.conf.vcru.apiHost + apiPath, {
-            method: 'POST',
-            body: params,
-            headers: {
-                'X-Device-Token': this.conf.vcru.apiToken,
-            },
-        }).then(res => {
-            responseHeaders = res.headers.raw();
-            return res.json();
-        });
-
-        const posToken = responseHeaders['X-Device-Possession-Token'.toLowerCase()];
-
-        if (!posToken) {
-            throw 'possess failed: result=' + JSON.stringify(result)
-                + '; responseHeaders=' + JSON.stringify(responseHeaders);
-        }
-
-        return posToken;
-    }
+    //     return {
+    //         id: result.result.id,
+    //         url: result.result.url,
+    //     };
+    // }
